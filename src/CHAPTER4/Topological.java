8c5eb1027297abcf937bb23a1fbd09fcfca202d3
Xiaohu <975326527@qq.com>
2020-08-11 23:40:33 +0800

    Create Topological.java

---
+++ src/CHAPTER4/Topological.java
@@ -0,0 +1,74 @@
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.EdgeWeightedDirectedCycle;
+import edu.princeton.cs.algs4.SymbolDigraph;
+import edu.princeton.cs.algs4.DepthFirstOrder;
+
+
+public class Topological {
+    private Iterable<Integer> order;
+    private int[] rank;
+    
+
+    public Topological(Digraph G)
+    {
+        DirectedCycle  finder = new DirectedCycle(G);
+        if(!finder.hasCycle())
+        {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+            rank = new int[G.V()];
+            int i=0;
+            for(int v:order)
+            {
+                rank[v] = i++;
+            }
+        }
+    }
+    public Topological(EdgeWeightedDigraph G)
+    {
+        EdgeWeightedDirectedCycle  finder = new EdgeWeightedDirectedCycle(G);
+        if(!finder.hasCycle())
+        {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+        }
+    }
+    public Iterable<Integer> order()
+    {
+        return order;
+    }
+    public boolean hasOrder()
+    {
+        return order != null;
+        
+    }
+    public boolean isDAG()
+    {
+        return hasOrder();
+    }
+    public int rank(int v)
+    {
+        validateVertex(v);
+        if(hasOrder()) return rank[v];
+        else return -1;
+        
+    }
+    private void validateVertex(int v)
+    {
+        int V = rank.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+    public static void main(String[] args)
+    {
+        String filename = args[0];
+        String delimiter = args[1];
+        SymbolDigraph sg = new SymbolDigraph(filename, delimiter);
+        Topological topological = new Topological(sg.digraph());
+        for(int v:topological.order())
+        {
+            StdOut.println(sg.nameOf(v));
+        }
+    }
+}
