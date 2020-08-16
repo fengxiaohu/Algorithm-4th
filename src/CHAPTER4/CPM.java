0b1eebb9b2dc4b97065817589efe8e61a5446535
Xiaohu <975326527@qq.com>
2020-08-16 16:03:02 +0800

    Update CPM.java
    add some explanations

--- src/CHAPTER4/CPM.java
+++ src/CHAPTER4/CPM.java
@@ -6,26 +6,26 @@ public class CPM {
     private CPM(){}
     public static void main(String[] args)
     {
-        int n = StdIn.readInt();
-        int source = 2*n;
-        int sink = 2*n+1;
-        EdgeWeightedDigraph G = new EdgeWeightedDigraph(2*n+2);
+        int n = StdIn.readInt(); // job number
+        int source = 2*n; // source point
+        int sink = 2*n+1; // end point
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(2*n+2);// construct edge-weighted digraph
         for(int i=0;i<n;i++)
         {
-            double duration = StdIn.readDouble();
-            G.addEdge(new DirectedEdge(source, i, 0.0));
-            G.addEdge(new DirectedEdge(i+n, sink, 0.0));
-            G.addEdge(new DirectedEdge(i, i+n, duration));
+            double duration = StdIn.readDouble(); // job cost
+            G.addEdge(new DirectedEdge(source, i, 0.0));// source point to All points
+            G.addEdge(new DirectedEdge(i+n, sink, 0.0)); // end points to all points
+            G.addEdge(new DirectedEdge(i, i+n, duration)); // self connected
             
-            int m = StdIn.readInt();
+            int m = StdIn.readInt();//related job
             for(int j =0;j<m;j++)
             {
-                int precedent = StdIn.readInt();
+                int precedent = StdIn.readInt(); // job priority
                 G.addEdge(new DirectedEdge(n+i, precedent, 0.0));
             }
 
         }
-        AcyclicLP lp = new AcyclicLP(G, source);
+        AcyclicLP lp = new AcyclicLP(G, source); // longtest path search in graph
         StdOut.println(" job   start  finish");
         StdOut.println("--------------------");
         for (int i = 0; i < n; i++) {
